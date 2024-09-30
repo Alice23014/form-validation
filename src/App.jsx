1c4e71cf305f2c26ad67591525dd6700de114808
Alice <alice@gmail.com>
2024-09-30 22:26:39 +0300

    independent counters

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import "./App.css";
-import Counter from "./Counter";
+import { useState } from "react";
 
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  function increment() {
+    return setCounter((prev) => prev + 1);
+  }
+  function decrement() {
+    if (counter <= 0) {
+      alert("Нельзя меньше нуля");
+      return;
+    }
+    return setCounter((prev) => prev - 1);
+  }
+  return (
+    <>
+      <h1>{counter}</h1>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+    </>
+  );
+}
 function App() {
-  
-
   return (
     <div className="App">
       <Counter/>
